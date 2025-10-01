e38416fc88d0df93356566a0c3afc6663a622824
Margarita Zhuk <zhukrita@gmail.com>
2025-10-01 23:50:51 +0300

    another conflict

--- src/main/java/zhuk/margarita/Main.java
+++ src/main/java/zhuk/margarita/Main.java
@@ -7,12 +7,12 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
-        System.out.printf("Conflict time!");
+        System.out.printf("Time for conflict!");
 
-        for (int c = 1; c <= 5; c++) {
+        for (int a = 1; a <= 5; a++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("c = " + c);
+            System.out.println("a = " + a);
         }
     }
 }
