306f2a3f773b507c3db790bad44b537e7522f7f6
Margarita Zhuk <zhukrita@gmail.com>
2025-10-01 23:54:19 +0300

    final conflict

--- src/main/java/zhuk/margarita/Main.java
+++ src/main/java/zhuk/margarita/Main.java
@@ -7,7 +7,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
-        System.out.printf("Time for conflict!");
+        System.out.printf("Conflict!");
 
         for (int a = 1; a <= 5; a++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
