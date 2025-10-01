6f9acaf2256a2af24af63cbf2655f2790010fd9f
MargaritaZhuk <45733303+MargaritaZhuk@users.noreply.github.com>
2025-10-01 23:31:33 +0300

    Update Main.java

--- src/main/java/zhuk/margarita/Main.java
+++ src/main/java/zhuk/margarita/Main.java
@@ -6,12 +6,12 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Time for conflict!");
 
-        for (int i = 1; i <= 5; i++) {
+        for (int a = 1; a <= 5; a++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            System.out.println("a = " + a);
         }
     }
 }
